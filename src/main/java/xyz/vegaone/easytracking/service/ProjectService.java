6c8ea968f53e9bdffa19223c775886372fe62f20
Bogdan Sabau <sabaubogdanvasile@gmail.com>
2018-01-01 18:28:35 +0200

    modified Project service class and created unit test for project class

--- src/main/java/xyz/vegaone/easytracking/service/ProjectService.java
+++ src/main/java/xyz/vegaone/easytracking/service/ProjectService.java
@@ -3,12 +3,18 @@ package xyz.vegaone.easytracking.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import xyz.vegaone.easytracking.domain.ProjectEntity;
+import xyz.vegaone.easytracking.domain.TaskEntity;
+import xyz.vegaone.easytracking.domain.UserStoryEntity;
 import xyz.vegaone.easytracking.dto.Project;
+import xyz.vegaone.easytracking.dto.Task;
 import xyz.vegaone.easytracking.dto.UserStory;
 import xyz.vegaone.easytracking.mapper.ProjectMapper;
+import xyz.vegaone.easytracking.mapper.UserStoryMapper;
 import xyz.vegaone.easytracking.repo.ProjectRepo;
+import xyz.vegaone.easytracking.repo.UserStoryRepo;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProjectService {
@@ -22,6 +28,12 @@ public class ProjectService {
     @Autowired
     private UserStoryService userStoryService;
 
+    @Autowired
+    private UserStoryRepo userStoryRepo;
+
+    @Autowired
+    private UserStoryMapper userStoryMapper;
+
     public Project createProject(Project project){
         ProjectEntity projectEntity = projectMapper.dtoToDomain(project);
         ProjectEntity savedProject = projectRepo.save(projectEntity);
@@ -33,14 +45,36 @@ public class ProjectService {
     }
 
     public Project getProject(Long id){
-        ProjectEntity projectEntity = projectRepo.getOne(id);
-        Project projectDto = projectMapper.domainToDto(projectEntity);
+//        ProjectEntity projectEntity = projectRepo.getOne(id);
+//        Project projectDto = projectMapper.domainToDto(projectEntity);
+//
+//        List<UserStory> userStoryList = userStoryService.findAllByProjectId(id);
+//
+//        projectDto.setUserStories(userStoryList);
+//
+//        return projectDto;
+        Optional<ProjectEntity> projectOptional = projectRepo.findById(id);
 
-        List<UserStory> userStoryList = userStoryService.findAllByProjectId(id);
+        if (projectOptional.isPresent()) {
+            ProjectEntity projectEntity = projectOptional.get();
 
-        projectDto.setUserStories(userStoryList);
+            Project project = projectMapper.domainToDto(projectEntity);
 
-        return projectDto;
+            Optional<UserStoryEntity> userStoryOptional = userStoryRepo.findById(projectEntity.getId());
+
+            UserStory userStory = null;
+
+            if (userStoryOptional.isPresent()) {
+                userStory = userStoryMapper.domainToDto(userStoryOptional.get());
+            }
+
+           // project.setUserStories(userStory);
+
+            return project;
+        }
+
+        return null;
+
     }
 
     public void deleteProject(Long id){
--- src/test/java/xyz/vegaone/easytracking/project/ProjectServiceTest.java
+++ src/test/java/xyz/vegaone/easytracking/project/ProjectServiceTest.java
@@ -26,6 +26,7 @@ public class ProjectServiceTest {
 
     public static final String PROJECT_DESCRIPTION="Project test";
     public static final String PROJECT_NAME="Project name";
+    public static final String PROJECT_NEW_NAME="Project new name";
 
     @Autowired
     private ProjectService projectService;
@@ -60,6 +61,59 @@ public class ProjectServiceTest {
         Assert.assertEquals("The project name should have matched", PROJECT_NAME, savedProject.getName());
         Assert.assertEquals("The project description should have matched", PROJECT_DESCRIPTION, savedProject.getDescription());
 
+    }
+
+    @Test
+    public void getProject(){
+        //given
+
+        Project project = new Project();
+        project.setDescription(PROJECT_DESCRIPTION);
+        project.setName(PROJECT_NAME);
+
+        Project savedProject = projectService.createProject(project);
+
+        //when
+        Project findProject = projectService.getProject(savedProject.getId());
+
+        //then
+        Assert.assertNotNull("There should have been one project in the database", findProject);
+        Assert.assertEquals("The project name should have matched", PROJECT_NAME, findProject.getName());
+        Assert.assertEquals("The project description should have matched", PROJECT_DESCRIPTION, findProject.getDescription());
+
+    }
+
+    @Test
+    public void deleteTask(){
+        //given
+        Project project = new Project();
+        project.setDescription(PROJECT_DESCRIPTION);
+        project.setName(PROJECT_NAME);
+
+        Project savedProject = projectService.createProject(project);
+
+        //when
+        projectService.deleteProject(savedProject.getId());
+        //then
+        Assert.assertNull("Project should have been deleted from db", projectService.getProject(savedProject.getId()));
+
+    }
+
+    @Test
+    public void updateProjectServiceTest(){
+        //given
+        Project project = new Project();
+        project.setDescription(PROJECT_DESCRIPTION);
+        project.setName(PROJECT_NAME);
+
+        Project savedProject = projectService.createProject(project);
+        //when
+
+        savedProject.setName(PROJECT_NEW_NAME);
+        Project updatedProject = projectService.updateProject(savedProject);
+        //then
+        Assert.assertNotNull("There should have been one task in the database", updatedProject);
+        Assert.assertEquals("The task title should have matched", PROJECT_NEW_NAME, updatedProject.getName());
     }
 
 }
