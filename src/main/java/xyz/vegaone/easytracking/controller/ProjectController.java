0ceae92a8de664dbebc008d9be117ceda419db48
Rus David <davidrus.art@gmail.com>
2018-01-09 23:19:49 +0200

    Create endpoint for Project

---
+++ src/main/java/xyz/vegaone/easytracking/controller/ProjectController.java
@@ -0,0 +1,48 @@
+package xyz.vegaone.easytracking.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import xyz.vegaone.easytracking.dto.Project;
+import xyz.vegaone.easytracking.service.ProjectService;
+
+
+@RestController
+@RequestMapping(value = "api/project")
+@Slf4j
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Project getProject(@PathVariable(value = "id") Long id) {
+        Project project = projectService.getProject(id);
+
+        return project;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Project createProject(@RequestBody Project project) {
+        Project createdProject = projectService.createProject(project);
+
+        return createdProject;
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Project updateProject(@RequestBody Project project) {
+        Project updatedProject = projectService.updateProject(project);
+
+        return updatedProject;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProject(@PathVariable(value = "id") Long id) {
+        projectService.deleteProject(id);
+    }
+}
