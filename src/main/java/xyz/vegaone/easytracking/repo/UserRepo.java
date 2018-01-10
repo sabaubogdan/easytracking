83e6d05fc625990c7e8cb47db8b05c4c3efb3ef5
Rus David <davidrus.art@gmail.com>
2018-01-10 22:03:55 +0200

    Add User data layer

---
+++ src/main/java/xyz/vegaone/easytracking/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package xyz.vegaone.easytracking.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import xyz.vegaone.easytracking.domain.UserEntity;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserEntity, Long>{
+
+}
