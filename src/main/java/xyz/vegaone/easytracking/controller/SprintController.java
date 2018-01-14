bab267b6f8729f565cf3a5a60811e4daabb43fd8
Bogdan Sabau <sabaubogdanvasile@gmail.com>
2018-01-14 19:24:41 +0200

    Added SprintController

---
+++ src/main/java/xyz/vegaone/easytracking/controller/SprintController.java
@@ -0,0 +1,54 @@
+package xyz.vegaone.easytracking.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import xyz.vegaone.easytracking.dto.Sprint;
+import xyz.vegaone.easytracking.service.SprintService;
+
+@RestController
+@RequestMapping(value = "/api/sprint")
+@Slf4j
+public class SprintController {
+
+    private SprintService sprintService;
+
+    @Autowired
+    public SprintController(SprintService sprintService) {
+        this.sprintService = sprintService;
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Sprint getSprint(@PathVariable(value = "id") Long id) {
+
+        Sprint sprint = sprintService.getSprint(id);
+
+        return sprint;
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Sprint createSprint(@RequestBody Sprint sprint) {
+        Sprint createdSprint = sprintService.createSprint(sprint);
+
+        return createdSprint;
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Sprint updateSprint(@RequestBody Sprint sprint) {
+        Sprint updatedSprint = sprintService.updateSprint(sprint);
+
+        return updatedSprint;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteSprint(@PathVariable(value = "id") Long id) {
+        sprintService.deleteSprint(id);
+
+    }
+}
